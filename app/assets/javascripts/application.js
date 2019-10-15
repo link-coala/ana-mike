55154dded755d8b996d69957443e801cb663302d
Your Name <you@example.com>
2019-10-15 21:01:06 +0000

    add_provisional_loadging_image_to_rteception

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,26 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-$(document).ready(function(){
- $(".titles").delay(1000).fadeOut();
-  
+
+//example
+//$(document).ready(function(){
+// $(".titles").delay(1000).fadeOut();
+//});
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
 });
