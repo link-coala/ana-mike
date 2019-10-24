405108ea325566d8e15633f25913feea571aab71
Your Name <you@example.com>
2019-10-24 19:15:23 +0000

    add_locale_to_all_pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
  around_action :switch_locale
- 
+ before_action :set_locale
+ private
+def set_locale
+  I18n.locale = params[:locale] if params[:locale].present?
+  # current_user.locale
+  # request.subdomain
+  # request.env["HTTP_ACCEPT_LANGUAGE"]
+  # request.remote_ip
+end
+def default_url_options(options = {})
+  {locale: I18n.locale}
+end
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
